255cdcaf3aef687719bbcc635ce66d5698b5e476
ParkerBeck <pbeck27@gmail.com>
2018-01-08 16:30:39 -0800

    Add start of ItemPage

---
+++ more-in-comments/src/components/ItemPage/ItemPage.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class ItemPage extends Component {
+
+    render() {
+        return (
+            <div>
+                <div>Name</div>
+                <div>List of links!</div>
+            </div>
+        );
+    }
+}
+
+export default ItemPage;
